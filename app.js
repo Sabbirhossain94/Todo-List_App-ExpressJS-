5943317bd0ff2728cfad5176bba93cfec3a9e3f9
Sabbirhossain94 <sabbirshawon1994@hotmail.com>
2021-09-28 21:13:30 +0600

    added mongo cloud

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-mongoose.connect("mongodb://localhost:27017/todolistDB",
+mongoose.connect("mongodb+srv://admin-sabbir:test123@cluster0.mklqs.mongodb.net/todolistDB",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
